2199e3cb3b1350d49d246742cea2144aab123c25
rutdaw <rutdaw@mat.umk.pl>
2025-01-31 21:06:06 +0100

    Dodano funkcje sprawdzajaca liczby pierwsze

--- projekt.cpp
+++ projekt.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
 
+bool czy_pierwsz(int n){
+    if(n < 2) return false;
+    for(int i = 2 ; i * i < = n; i ++ ){
+        if(n % i==0)return false;
+
+    }
+    return true;
+
+
+}
+
 int main(){
     int value;
     std::cin>>value;
-    std::cout<<value<<std::endl;
+    std::cout<<value<<" jest "<< (czypierwsza(value)? "liczba pierwsza" : "nie jest liczba pierwsza")<<<std::endl;
     return 0;
 }
